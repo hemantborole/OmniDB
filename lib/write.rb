3091e348e251d2cccecffc4dae15eaea64e9f7d9
Hemant Borole <hemant@dungeon.(none)>
2009-05-19 09:13:47 -0700

    Added methods to write to db and cache

--- lib/write.rb
+++ lib/write.rb
@@ -3,21 +3,36 @@ require File.join(lib_dir, 'db_utils')
 
 class Writer
 
+	CACHE_OBJECT_SIZE = 5000
+	LIFE_TIME = 1800 # seconds
 	attribute_accessor :db_handle
 
-	LIFE_TIME = 1800 # seconds
-	def db_write(key, value, life_time = LIFE_TIME)
-		@dbhandle ||= DbUtils.connect_to_db(dbname)
-		end_ts = Time.now + life_time
-		connection << "insert into cache values( #{@id}, #{key}, #{value})"
-		@id += 1
+	public
+	def atomic_write(key, value, life_time = LIFE_TIME)
+		 Mutex.new.synchronize do
+			async_write(key, value, life_time)
+		 end
 	end
 
-	def cache_write
-		
+	def async_write(key, value, life_time = LIFE_TIME)
+		 threads = []
+		 threads << Thread.new { db_write(key, value, life_time) }
+		 threads << Thread.new { cache_write(key, value, life_time) }
+		 threads.each {|t| t.join }
 	end
 
 	private
-	@id = 0
+
+	def db_write(key, value, life_time = LIFE_TIME)
+		@dbhandle ||= DbUtils.connect_to_db(dbname)
+		end_ts = Time.now + life_time
+		connection << "insert into cache('name','value','end_ts','create_by') values( #{key}, #{value}, #{end_ts}, 'dummy' )"
+	end
+
+	def cache_write(key, value, life_time = LIFE_TIME)
+		return if value.size > 5000
+		return if life_time <= 0
+		DHTCache.hash_in(key, value, life_time)
+	end
 
 end
