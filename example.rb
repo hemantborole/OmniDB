9116b91ec22f82a68ffe1919b96b4b15e717f1cf
Hemant Borole <hborole@YPCMC01510.local>
2009-05-19 12:31:56 -0700

    Read back the value from the cache.

--- example.rb
+++ example.rb
@@ -8,6 +8,8 @@ if ARGV.length == 2
 	p "Adding entry with key => #{key} and value => #{value}"
 	w = Writer.new
 	w.atomic_write(key, value)
+  
+  p "Getting value from cache : #{DHTCache.hash_out(key)}"
 else
 	puts "Usage: example.rb <key> <value>"
 end
